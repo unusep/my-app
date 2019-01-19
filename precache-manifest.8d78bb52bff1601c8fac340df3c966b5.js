4d4a9ed8d79e1421e5f5a61dccf126dea0e80cdd
Deshun Cai <unusep@gmail.com>
2019-01-19 14:54:46 +0800

    Updates

---
+++ precache-manifest.8d78bb52bff1601c8fac340df3c966b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfafdbcef3867e920ac3",
+    "url": "/my-app/static/css/main.78b93204.chunk.css"
+  },
+  {
+    "revision": "cfafdbcef3867e920ac3",
+    "url": "/my-app/static/js/main.cfafdbce.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/my-app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62eb941605206645f7018c3f3d9bf617",
+    "url": "/my-app/index.html"
+  }
+];
